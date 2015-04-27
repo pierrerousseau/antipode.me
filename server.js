2d8b3b5d581da77a540d2d19080576069425c9c4
Pierre <pierre@antipode.me>
2015-04-27 09:55:41 +0200

    test port 80

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require("http");
-var port = process.env['PORT'] || 8080;
+var port = process.env['PORT'] || 80;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
