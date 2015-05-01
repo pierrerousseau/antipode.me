234cd942de51fc6938f0bf4f2059caf58e84eaa1
Pierre <pierre@antipode.me>
2015-05-01 10:34:13 +0200

    use openlayers library

--- public/scripts/antipode.me.js
+++ public/scripts/antipode.me.js
@@ -1,15 +1,67 @@
-function findParam(name) {
-    var regexp = new RegExp("[&?]" + name + "=([^&]+)"),
-        search = regexp.exec(location.search),
-        value;
+/* globals */
+var currentPosition = [0, 0];
 
-    try {
-        value = parseInt(search[1]);
-    } catch (e) {}
-
-    return value;
+/* antipode */
+function antipodeLongitude(longitude) {
+    return longitude > 180 ? longitude - 180 : longitude + 180;
 }
 
+function antipodeLatitude(latitude) {
+    return - latitude;
+}
+
+/* OL */
+function olPosition(longitude, latitude) {
+    return ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857');
+}
+
+function llPosition(position) {
+    return ol.proj.transform(position, 'EPSG:3857', 'EPSG:4326');
+}
+
+/* move */
+function move(to, map, duration, $marker) {
+    var pan,
+        marker;
+
+    pan = ol.animation.pan({
+        "duration": duration,
+        "source": currentPosition
+    });
+    map.beforeRender(pan);
+    map.getView().setCenter(to);
+    currentPosition = to;
+
+    
+    if ($marker && $marker.length) {
+        marker = new ol.Overlay({
+            positioning: 'center-center',
+            element: $marker[0],
+            position: to
+        });
+        map.addOverlay(marker);
+    }
+}
+
+function setPositionOnMap(map, position) {
+    var longitude = position[0],
+        latitude  = position[1],
+        start     = olPosition(longitude, latitude),
+        antipode  = olPosition(antipodeLongitude(longitude), 
+                               antipodeLatitude(latitude)),
+        timer,
+        duration  = 3000,
+        $you      = $("#you"),
+        $antipode = $("#antipode");
+
+        move(start, map, duration, $you);
+        timer = setTimeout(function () { 
+            clearTimeout(timer);
+            move(antipode, map, duration, $antipode); 
+        }, duration);
+}
+
+/* form */
 function cleanNumber(num) {
     num = parseInt(num);
 
@@ -19,71 +71,85 @@ function cleanNumber(num) {
     return num;
 }
 
-function setPositionOnMap() {
-    var longitude = cleanNumber($("input[name=longitude]").val()) + 180,
-        latitude  = - cleanNumber($("input[name=latitude]").val()),
-        $map      = $("iframe"),
-        mapUri    = $map.attr("src"),
-        paramSep  = "=",
-        uriSep    = "%2C";
+function formPosition() {
+    var $longitude = $("input[name=longitude]"),
+        $latitude  = $("input[name=latitude]"),
+        longitude  = cleanNumber($longitude.val()),
+        latitude   = cleanNumber($latitude.val());
 
-    console.log(longitude)
-    console.log(latitude)
-    mapUri = mapUri.split(paramSep);
-    console.log(mapUri)
-    mapUri[mapUri.length - 1] = "" + latitude + uriSep + longitude;
-    minLatitude  = latitude - 25;
-    minLongitude = longitude - 25;
-    maxLatitude  = latitude + 25;
-    maxLongitude = longitude + 25;
-    mapUri[1] = [
-        minLongitude, 
-        minLatitude, 
-        maxLongitude, 
-        "" + maxLatitude + "&layer"].join(uriSep);
-    console.log(mapUri)
-    mapUri = mapUri.join(paramSep);
-    $map.attr("src", mapUri);
+    $longitude.val(longitude);
+    $latitude.val(latitude);
+
+    return [longitude, latitude];
 }
 
-function askPosition() {
-    var watcher = navigator
-        .geolocation
-        .watchPosition(function (position) {
-            var longitude = position.coords.longitude,
-                latitude = position.coords.latitude;
-            navigator.geolocation.clearWatch(watcher);
-            setParams(longitude, latitude);
-        },
-        function () {},
-        { "enableHighAccuracy": true,
-            "timeout": 10000,
-            "maximumAge": 0 });
+/* geolocation */
+function geolocate(map) {
+    var $error = $("#geo-error"),
+        geolocation = new ol.Geolocation({
+            "projection": map.getView().getProjection(),
+            "trackingOptions": {
+                "enableHighAccuracy": true,
+                "timeout": 10000,
+                "maximumAge": 1000
+            },
+            "tracking": true
+    });
+
+    /* FixMe: a loader waiting change */
+    geolocation.once("change", function(evt) {
+        var position = llPosition(geolocation.getPosition());
+
+        /* FixMe: function to get and set inputs */
+        $("input[name=longitude]").val(position[0]);
+        $("input[name=latitude]").val(position[1]);
+        setPositionOnMap(map, position);
+    });
+
+    geolocation.on("error", function () {
+        $error.html("Please activate geolocalisation or et your coordinates");
+    });
+
+    return geolocation;
 }
 
-function setParams(longitude, latitude) {
-    var $longitudeInput = $("input[name=longitude]"),
-        $latitudeInput  = $("input[name=latitude]");
+/* main */
+function newMap(startPosition, zoom) {
+    var map, 
+        view; 
 
-    if (!longitude) {
-        longitude = findParam("longitude");
-    }
-    if (!latitude) {
-        latitude = findParam("latitude");
-    }
-    if (!longitude || ! latitude) {
-        position = askPosition();
-    }
-    $longitudeInput.val(longitude);
-    $latitudeInput.val(latitude);
+    view = new ol.View({
+        "center": startPosition,
+        "zoom": zoom
+    });
+    map = new ol.Map({
+        "target": "map",
+        "loadTilesWhileAnimating": true,
+        "layers": [
+            new ol.layer.Tile({
+                "source": new ol.source.OSM(),
+            })
+        ],
+        "view": view
+    });
 
-    setPositionOnMap();
+    return map;
 }
 
 $(document).ready(function () {
-    setParams();
+    var startPosition = currentPosition,
+        zoom          = 5,
+        map           = newMap(startPosition, zoom),
+        geolocation;
+
+    geolocation = geolocate(map);
     $("form").on("submit", function () {
-        setPositionOnMap();
+        var $error = $("#geo-error");
+
+        setPositionOnMap(map, formPosition());
+        $error.html("");
+
         return false;
     });
+
 });
